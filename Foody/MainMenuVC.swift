3eb09af8875464a951bc4bc8f254f97b5e72e713
Sebastian Strus <sebastian.strus1@gmail.com>
2017-10-31 00:17:42 +0100

    first commit

---
+++ Foody/MainMenuVC.swift
@@ -0,0 +1,35 @@
+//
+//  MainMenuVC.swift
+//  Foody
+//
+//  Created by Sebastian Strus on 2017-10-30.
+//  Copyright © 2017 Sebastian Strus. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
