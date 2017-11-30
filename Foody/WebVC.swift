e52029f595ea2235ae69c3c03137ab2f6ed134d2
Sebastian Strus <sebastian.strus1@gmail.com>
2017-11-30 23:52:03 +0100

    Youtube video

---
+++ Foody/WebVC.swift
@@ -0,0 +1,45 @@
+//
+//  WebVC.swift
+//  Foody
+//
+//  Created by Sebastian Strus on 2017-11-30.
+//  Copyright © 2017 Sebastian Strus. All rights reserved.
+//
+
+import UIKit
+
+class WebVC: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        getVideo(videoCode: "RmHqOSrkZnk")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func getVideo(videoCode:String) {
+        let url = URL(string: "http://www.youtube.com/embed/\(videoCode)")
+        webView.loadRequest(URLRequest(url: url!))
+        
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
